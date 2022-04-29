eb0c883c1e26d6c1d61b946833326ff820d81388
balabasta12 <balabasta12@gmail.com>
2022-04-29 21:07:20 +0300

    Готово

---
+++ logistic/views.py
@@ -0,0 +1,26 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
+from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.viewsets import ModelViewSet
+
+from logistic.filtres import StockFilter
+from logistic.models import Product, Stock
+from logistic.serializers import ProductSerializer, StockSerializer
+
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    # при необходимости добавьте параметры фильтрации
+    filterset_fields = ['title', 'description']
+    filter_backends = [SearchFilter]
+    pagination_class = LimitOffsetPagination
+
+
+class StockViewSet(ModelViewSet):
+    queryset = Stock.objects.all()
+    serializer_class = StockSerializer
+    pagination_class = LimitOffsetPagination
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = StockFilter
+    # при необходимости добавьте параметры фильтрации
